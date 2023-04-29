b4001495d330a61ed53ae663866df7dc7e11ffcc
varcario <ismar.pereira@gmail.com>
2023-04-29 17:42:45 -0500

    added openai api

--- src/components/messages.js
+++ src/components/messages.js
@@ -1,39 +1,56 @@
 import log from '../services/log-service'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Button, Card, Container, FormControl, InputGroup, Row } from 'react-bootstrap'
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: "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",
+});
+const openai = new OpenAIApi(configuration);
 
 export default function Messages() {
-    
+
     const name = Messages.name
     log.info(`${name} component being creation`)
     const title = "Messages"
-    const chatContainer = document.getElementById('chat-container');
-    const messageContainer = document.getElementById('message-container');
     const [inputMessage, setInputMessage] = useState("")
-    const [outputMessageQueue, setOutputMessageQueue] = useState({list:[]})
+    const [outputMessageQueue, setOutputMessageQueue] = useState({ list: [] })
 
-    const postMessage = () => {
+    const postMessage = async () => {
         log.info(`${name} enter key pressed`)
         const nextId = outputMessageQueue.list.length + 1
-        
-        console.log("POST -> inputMessage", inputMessage)
+        const prompt = inputMessage
+        log.info(`${name}POST -> inputMessage`, inputMessage)
         outputMessageQueue.list.push({
             id: nextId,
             source: "human",
-            text: inputMessage
+            text: prompt
         })
-        
-        setOutputMessageQueue({list:outputMessageQueue.list})
+        setOutputMessageQueue({ list: outputMessageQueue.list })
         setInputMessage("")
+       
+        const completion = await openai.createCompletion({
+            model: "text-davinci-003",
+            prompt: prompt,
+            temperature: 0.6,
+          });
+        
+        const nextBotId = outputMessageQueue.list.length + 1
+        const promptResponse = completion.data.choices[0].text
+        outputMessageQueue.list.push({
+            id: nextBotId,
+            source: "bot",
+            text: promptResponse
+        })
+        setOutputMessageQueue({ list: outputMessageQueue.list })
     }
 
     const onKeyDownHandler = (event) => {
-        if(event.key === "Enter")
-        {
+        if (event.key === "Enter") {
             postMessage()
         }
     }
-    const onClickHandler = ()=>{
+    const onClickHandler = () => {
         postMessage()
     }
 
@@ -44,54 +61,54 @@ export default function Messages() {
                 {title}
             </div>
         </Row>
-        <Row className="card rounded-0 border-0 h-100" style={{ padding: "0 0 4rem 0", margin: "0 0 -4rem 0" }} >
-            <Container 
-                className="me-0 m-0 h-100 d-flex flex-column justify-content-center align-items-center">
-                <Card 
-                    className="me-0 m-0 h-100 overflow-auto rounded-0" 
-                    style={{flexDirection: "column-reverse", minWidth: "48rem", maxWidth: "48rem"}}>
+        <Row className="card rounded-0 border-0 h-100 d-flex flex-column justify-content-center align-items-center " style={{ padding: "0 0 4rem 0", margin: "0 0 -4rem 0" }} >
+            <Container
+                className="me-0 m-0 h-100 col-sm-6">
+                <Card
+                    className="me-0 m-0 h-100 w-100 overflow-auto rounded-0"
+                    style={{ flexDirection: "column-reverse" }}>
                     <Card.Body >
-                    {   
-                        outputMessageQueue.list.map(m=>{
-                            if(m.source === "bot")
-                                return(
+                        {
+                            outputMessageQueue.list.map(m => {
+                                if (m.source === "bot")
+                                    return (
                                         <div key={m.id} className="d-flex align-item-baseline mb-4">
                                             <div className="pe-2">
                                                 {m.text}
                                             </div>
                                         </div>
                                     )
-                            else
-                                return(
-                                    <div key={m.id} className="d-flex align-item-baseline mb-4 text-end justify-content-end">
-                                        <div className="pe-2">
-                                            {m.text}
+                                else
+                                    return (
+                                        <div key={m.id} className="d-flex align-item-baseline mb-4 text-end justify-content-end">
+                                            <div className="pe-2">
+                                                {m.text}
+                                            </div>
                                         </div>
-                                    </div>
-                                )
-                        })
-                    } 
+                                    )
+                            })
+                        }
                     </Card.Body>
                 </Card>
             </Container>
         </Row>
-        <Row className="card rounded-0 border-1 m-0 p-1 position absolute" style={{ background: "#FEFEFE", height: "4rem", border: "1px solid #EAECED", margin: "0 5rem 0 0" }} >
-            <Container className="d-flex flex-column justify-content-center align-items-center">
-                <Card style={{minWidth: "48rem", maxWidth: "48rem"}}>
+        <Row className="card rounded-0 border-1 m-0 p-1 position absolute d-flex flex-column justify-content-center align-items-center" style={{ background: "#FEFEFE", height: "4rem", border: "1px solid #EAECED", margin: "0 5rem 0 0" }} >
+            <Container className="col-sm-6">
+                <Card >
                     <Card.Footer className="bg-transparent  border-0 bottom-0 m-0 p-0 align-item-baseline">
                         <InputGroup>
-                            <FormControl 
-                                type="text" 
+                            <FormControl
+                                type="text"
                                 tabIndex="0"
-                                value={inputMessage} 
-                                onChange={event=>{log.info(`${name} ${event.target.value}`); setInputMessage(event.target.value)}}
+                                value={inputMessage}
+                                onChange={event => { log.info(`${name} ${event.target.value}`); setInputMessage(event.target.value) }}
                                 onKeyDown={onKeyDownHandler}
-                                ></FormControl>
+                            ></FormControl>
                             <InputGroup.Text className="bg-transparent b-0">
-                                <Button 
-                                    variant="primary" 
+                                <Button
+                                    variant="primary"
                                     onClick={onClickHandler}
-                                    >send</Button>
+                                >send</Button>
                             </InputGroup.Text>
                         </InputGroup>
                     </Card.Footer>
